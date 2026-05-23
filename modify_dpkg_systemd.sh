1f08b55be6135675d01cd4a5869b4ad536bef693
yesimxev <3924854-yesimxev@users.noreply.gitlab.com>
2026-05-23 01:47:41 +0100

    systemd patch - use downgrade

--- modify_dpkg_systemd.sh
+++ modify_dpkg_systemd.sh
@@ -1,17 +1,17 @@
-#!/usr/bin/env bash
-if [ "$EUID" -ne 0 ]; then
-	echo "Run it as root"
-	exit 1
-fi
-file="/var/lib/dpkg/info/systemd.postinst"
-if [ ! -f "$file" ]; then
-	echo "$file not found"
-	exit 1
-fi
-setline="$(grep -n 'set -e' $file | cut -d : -f 1 | head -n 1)"
-exitline=$(($setline + 1))
-sed -i "${exitline}i exit 0" $file
-chmod 755 $file
-dpkg --configure -a
-apt --fix-broken install
-apt-mark hold systemd systemd-sysv libpam-systemd udev
+#!/bin/bash
+wget http://old.kali.org/kali/pool/main/s/systemd/udev_259.1-1_arm64.deb
+wget http://old.kali.org/kali/pool/main/s/systemd/libudev-dev_259.1-1_arm64.deb
+wget http://old.kali.org/kali/pool/main/s/systemd/libsystemd-shared_259.1-1_arm64.deb
+wget http://old.kali.org/kali/pool/main/s/systemd/libudev1_259.1-1_arm64.deb
+wget http://old.kali.org/kali/pool/main/s/systemd/libpam-systemd_259.1-1_arm64.deb
+wget http://old.kali.org/kali/pool/main/s/systemd/systemd-sysv_259.1-1_arm64.deb
+wget http://old.kali.org/kali/pool/main/s/systemd/libsystemd-dev_259.1-1_arm64.deb
+wget http://old.kali.org/kali/pool/main/s/systemd/libsystemd0_259.1-1_arm64.deb
+wget http://old.kali.org/kali/pool/main/s/systemd/systemd-timesyncd_259.1-1_arm64.deb
+wget http://old.kali.org/kali/pool/main/s/systemd/libnss-systemd_259.1-1_arm64.deb
+wget http://old.kali.org/kali/pool/main/s/systemd/systemd_259.1-1_arm64.deb
+wget http://old.kali.org/kali/pool/main/s/systemd/systemd-cryptsetup_259.1-1_arm64.deb
+dpkg -i *.deb
+apt install -f -y
+apt-mark hold udev libpam-systemd libsystemd-shared systemd-sysv libsystemd-dev libsystemd0 systemd-timesyncd libnss-systemd libudev1 libudev-dev systemd systemd-cryptsetup systemd-timesyncd
+
